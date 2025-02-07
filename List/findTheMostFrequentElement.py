a74f5b626ace8f5211c9bbdfbe68b895efc2eece
Bharat-Gadhave <bharatgadhave777@gmail.com>
2025-02-07 22:11:37 +0530

    added new def/function in given file

--- List/findTheMostFrequentElement.py
+++ List/findTheMostFrequentElement.py
@@ -1,15 +1,27 @@
 numbers = [1,2,3,3,3,4,4,5,5,5,5,5,5,5,6,33,2,1,2,3,4]
 
-def mostFrequent(lst):
-    maxCount = 0
-    mostFreq = None
+# def mostFrequent(lst):
+#     maxCount = 0
+#     mostFreq = None
 
-    for item in lst:
-        count = lst.count(item)
-        if count > maxCount:
-            maxCount = count
-            mostFreq = item
+#     for item in lst:
+#         count = lst.count(item)
+#         if count > maxCount:
+#             maxCount = count
+#             mostFreq = item
         
-    return mostFreq
+#     return mostFreq
     
-print(mostFrequent(numbers))
+# print(mostFrequent(numbers))
+
+
+
+# =============================================
+# Remove Dublicate elements from list
+
+
+def removeDublicate(lst):
+    uniqueItems = list(set(lst))
+    return uniqueItems
+
+print(removeDublicate(numbers))
